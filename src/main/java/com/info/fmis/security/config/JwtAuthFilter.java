78fa59dd40313650e2492f4a8f5993e331ea8f9e
sulaimanallemyar <sulaiman.allemyar@gmail.com>
2023-09-18 15:20:32 +0430

    add a custom message for jwt token expired change java version to 11

--- src/main/java/com/info/fmis/security/config/JwtAuthFilter.java
+++ src/main/java/com/info/fmis/security/config/JwtAuthFilter.java
@@ -10,10 +10,14 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.info.fmis.service.JwtService;
 import com.info.fmis.service.UserService;
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 // This class helps us to validate the generated jwt token
 @Component
@@ -26,19 +30,38 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 	private UserService userDetailsService;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
 		String authHeader = request.getHeader("Authorization");
 		String token = null;
 		String username = null;
 		if (authHeader != null && authHeader.startsWith("Bearer ")) {
 			token = authHeader.substring(7);
+
+			DecodedJWT jwt = null;
+
+			try {
+				jwt = JWT.decode(token);
+			} catch (JWTDecodeException e) {
+				System.out.println("exception while decoding token");
+			}
+
+			if (jwt.getExpiresAt().before(new Date())) {
+
+				final String expiredMsg = (String) request.getAttribute("expired");
+				final String msg = (expiredMsg != null) ? expiredMsg : "Token Expired";
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, msg);
+				return;
+			}
+
 			username = jwtService.extractUsername(token);
 		}
 
 		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 			if (jwtService.validateToken(token, userDetails)) {
-				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+						null, userDetails.getAuthorities());
 				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 				SecurityContextHolder.getContext().setAuthentication(authToken);
 			}
